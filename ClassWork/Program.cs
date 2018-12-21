d45727a66e3fc56eab407d5ca4a8252841a85bbc
turmakhanovD <46023441+turmakhanovD@users.noreply.github.com>
2018-12-21 18:22:32 +0600

    Add files via upload

---
+++ ClassWork/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 task
+            //int number;
+            //int x1, x2;
+            //int a = 7;
+            //int b = 3;
+            //int c = 4;
+
+            //Console.Write("Enter number: ");
+            //number = int.Parse(Console.ReadLine());
+
+
+            //double formula = c * c + 4 * a * b;
+            //double sqrt = Math.Sqrt(formula);
+
+            //if (formula < 0)
+            //    Console.WriteLine("Error, sqrt from this x lower than 0!");
+            //else if (formula > 0)
+            //{
+            //    x1 = (3 + (int)sqrt) / 2;
+            //    x2 = (3 - (int)sqrt) / 2;
+            //    Console.WriteLine("X1 = {0}",x1);
+            //    Console.WriteLine("X2 = {0}",x2);
+            //}
+
+            //Console.ReadLine();
+
+
+            //2 task
+            //int radius;
+            //Console.Write("Enter number: ");
+            //radius = int.Parse(Console.ReadLine());
+
+            //double C = 2 * Math.PI * radius;
+            //double S = Math.PI * (radius * radius);
+
+            //Console.WriteLine("Lenght: {0}", C);
+            //Console.WriteLine("S: {0}", S);
+
+            //Console.ReadLine();
+
+
+            //task 3
+            //int sm;
+            //int metr = 0;
+
+            //Console.Write("Enter number: ");
+            //sm = int.Parse(Console.ReadLine());
+
+            //while (sm >= 0)
+            //{
+            //    if (sm - 100 >= 0)
+            //    {
+            //        metr += 1;
+            //        sm -= 100;
+            //    }
+            //    else break;
+            //}
+
+            //Console.Write("there is {0}",metr);
+
+
+            //Console.ReadLine();
+
+
+            //task 4
+            //int dayOfWeek;
+            //int week = 7;
+            //int daysFromThis = 234;
+            //int result = 0;
+
+            //Console.Write("Enter number: ");
+            //dayOfWeek = int.Parse(Console.ReadLine());
+
+            //result = (dayOfWeek + daysFromThis) / week;
+
+            //Console.WriteLine("There are: {0}", result);
+            //Console.ReadLine();
+
+            //task 5
+            //int ten = 10;
+            //int one = 1;
+            //int ourNumber;
+            //int composition;
+            //int sum;
+
+            //Console.Write("Enter number: ");
+            //ourNumber = int.Parse(Console.ReadLine());
+
+            //sum = (ourNumber / 10) + (ourNumber % 10);
+            //composition = (ourNumber / 10) * (ourNumber % 10);
+
+            //one = ourNumber % ten;
+            //ten = ourNumber / ten;
+
+
+            //Console.WriteLine("Composition: {0}", composition);
+            //Console.WriteLine("Sun: {0}",sum);
+            //Console.WriteLine("Tens: {0}", ten);
+            //Console.WriteLine("Ones: {0}", one);
+
+            //Console.ReadLine();
+
+            //task 6
+            //int ourNumber;
+            //int sum;
+            //int composition;
+
+            //Console.Write("Enter number: ");
+            //ourNumber = int.Parse(Console.ReadLine());
+
+            //sum = (ourNumber / 1000) + ((ourNumber / 100)% 10) + ((ourNumber / 10)% 10) + (ourNumber % 10);
+            //composition = (ourNumber / 1000) * ((ourNumber / 100) % 10) * ((ourNumber / 10) % 10) * (ourNumber % 10);
+
+            //Console.WriteLine("{0}", sum);
+            //Console.WriteLine("{0}", composition);
+
+            //Console.ReadLine();
+
+            //task 7
+            //int x = 456;
+            //int num = 45;
+            //int result;
+            //result = (x  % 10);
+            //result = 405 + (result * 10);
+
+            //Console.WriteLine("result: {0}", result);
+            //Console.ReadLine();
+
+            //task 8
+            //bool x, y;
+
+            //Console.Write("Enter 1 number: ");
+            //x = bool.Parse(Console.ReadLine());
+
+            //Console.Write("Enter 2 number: ");
+            //y = bool.Parse(Console.ReadLine());
+
+            //string strings = "!" + x + "&&" + "!" + y;
+            //string strings1 =  x + "||" + "(!" + x + "!" + y +")";
+            //string strings2 = "(" + "!" + x + "!" + y + ")" + "||" + y;
+            //Console.WriteLine(strings);
+            //Console.WriteLine(strings1);
+            //Console.WriteLine(strings2);
+            //Console.ReadLine();
+
+        }
+    }
+}
